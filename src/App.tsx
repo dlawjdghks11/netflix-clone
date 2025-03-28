9765a3ee10b31f5d3525634a514ab6dc65859f72
dlawjdghks11 <85840643+dlawjdghks11@users.noreply.github.com>
2025-03-28 13:38:41 +0900

    initial published

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Search from "./pages/Search";
+import Tv from "./pages/Tv";
+import Header from "./components/Header";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/tv" element={<Tv />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
